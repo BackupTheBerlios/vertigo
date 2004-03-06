ea8c9194da33ba64b0dd493c0e6da7e8dc2f19a2
smt <smt>
2004-03-06 17:29:02 +0000

    seperate containterview

---
+++ vertigo/vertigo/fe-kde/containerview.cpp
@@ -0,0 +1,33 @@
+#include <kdebug.h>
+
+#include "containerview.h"
+#include "mainwindow.h"
+
+ContainerView::ContainerView (QWidget * parent, const char *name, MainWindow *w, server *s) : QWidget (parent, name, WDestructiveClose)
+{
+    setWindow(w);
+        setServer(s);
+}
+
+void ContainerView::setWindow(MainWindow *w)
+{
+    kdDebug() << "container setwindow"<<endl;
+    m_window=w;
+}
+
+void ContainerView::showView()
+{
+        if (window())
+                window()->showView(this);
+}
+
+void ContainerView::closeView()
+{
+        close();
+}
+
+
+MainWindow* ContainerView::window()
+{
+    return m_window;
+}
---
+++ vertigo/vertigo/fe-kde/containerview.h
@@ -0,0 +1,32 @@
+#ifndef ContainerWin_H
+#define ContainerWin_H
+
+#include <qwidget.h>
+
+
+#include "../common/xchat.h"
+
+class MainWindow;
+
+class ContainerView: public QWidget
+{
+    Q_OBJECT
+
+    public:
+        ContainerView (QWidget* parent = 0, const char* name = 0,  MainWindow *w=0, server*s=0);
+        virtual void setWindow(MainWindow *w);
+        virtual MainWindow* window();
+
+        server *getServer(){return m_server;}
+        void setServer(server *s){m_server=s;}
+
+    public slots:
+        virtual void showView();
+        virtual void closeView();
+
+    private:
+        MainWindow *m_window;
+        server *m_server;
+};
+
+#endif
